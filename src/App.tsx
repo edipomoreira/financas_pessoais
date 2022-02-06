c6cbc39c802d620d94876ecff2086b05a2530add
edipomoreira <edipo.ap.moreira@outlook.com>
2022-02-06 16:28:35 -0300

    added infoArea

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,33 @@ const App = () => {
   const [list, setList] = useState(items);
   const [filteredList, setFilteredList] = useState<Item[]>([])
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
 
-  useEffect(()=>{
+
+  useEffect(()=> {
     setFilteredList(filterListByMonth(list, currentMonth));
   }, [list, currentMonth]);
+
+  useEffect(()=> {
+    let incomeCount = 0;
+    let expenseCount = 0;
+
+    for(let i in filteredList) {
+      if(categories[filteredList[i].category].expense) {
+        expenseCount += filteredList[i].value;
+      } else{
+        incomeCount += filteredList[i].value;
+      }
+    }
+    setIncome(incomeCount);
+    setExpense(expenseCount);
+
+  }, [filteredList]);
+
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth)
+  } 
 
   return (
   <C.Container>
@@ -26,7 +49,12 @@ const App = () => {
     </C.Header>
     <C.Body>
 
-    <InfoArea currentMonth={currentMonth}/>
+    <InfoArea 
+        currentMonth={currentMonth}
+        onMonthChange={handleMonthChange}
+        income={income}
+        expense={expense}
+    />
 
     {/* Area de inserção */}
 
--- src/components/InfoArea/styles.ts
+++ src/components/InfoArea/styles.ts
@@ -7,30 +7,30 @@ export const Container = styled.div`
     border-radius: 10px;
     margin-top: -40px;
     display: flex;
-    align-items: center
+    align-items: center;
 `;
 
 export const MonthArea = styled.div`
     flex: 1;
     display: flex;
-    align-items: center
-`;
-
-export const ResumeArea = styled.div`
-    flex: 2;
-    display: flex;
+    align-items: center;
 `;
 
 export const MonthArrow = styled.div`
     width: 40px;
     text-align: center;
     font-size: 25px;
-    cursor: pointer
+    cursor: pointer;
 `;
 
 export const MonthTitle = styled.div`
     flex: 1;
     text-align: center;
+`;
+
+export const ResumeArea = styled.div`
+    flex: 2;
+    display: flex;
 `;
 
 
